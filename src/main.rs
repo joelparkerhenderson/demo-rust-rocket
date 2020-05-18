af2bfb25ec315c6d667b12e7cea4b0653e0c4ebd
Joel Parker Henderson <joel@joelparkerhenderson.com>
2020-05-18 16:40:05 -0700

    Add pages

--- src/main.rs
+++ src/main.rs
@@ -2,6 +2,32 @@
 
 #[macro_use] extern crate rocket;
 
+// Rocket makes it easy to serve pages by using "multiple segments".
+// You can match against multiple segments by using <param..> in a 
+// route path. 
+//
+// The type of such parameters, known as segments guards, must implement
+// FromSegments. A segments guard must be the final component of a path: 
+// any text after a segments guard will result in a compile-time error.
+//
+// Use paths and named files:
+//
+//     use std::path::PathBuf;
+//     use rocket::response::NamedFile;
+//
+// Create a route and function:
+//
+//     #[get("/page/<path..>")]
+//     fn pages(path: PathBuf) { /* ... */ }
+//
+use std::path::{Path,PathBuf};
+use rocket::response::NamedFile;
+
+#[get("/pages/<path..>")]
+fn pages(path: PathBuf) -> Option<NamedFile> {
+    NamedFile::open(Path::new("pages/").join(path)).ok()
+}
+
 // Rocket makes it easy to serve static files. Use the StaticFiles custom 
 // handler from rocket_contrib, which makes it as simple as one line.
 //
@@ -15,7 +41,7 @@
 //
 // Edit the main fucntion and mount the route:
 //
-//     rocket.mount("/public", StaticFiles::from("/static"))
+//     rocket.mount("/", StaticFiles::from("/files"))
 //
 // See https://api.rocket.rs/v0.4/rocket_contrib/serve/struct.StaticFiles.html
 //
@@ -28,7 +54,7 @@ fn index() -> &'static str {
 
 fn main() {
     rocket::ignite()
-    .mount("/", routes![index])
-    .mount("/public", StaticFiles::from("/static"))
+    .mount("/", routes![index, pages])
+    .mount("/", StaticFiles::from("/files"))
     .launch();
 }
