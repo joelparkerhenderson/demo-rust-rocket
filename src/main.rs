3c8b8506f6f095e7fc5baedb483c84b796de5d24
Joel Parker Henderson <joel@joelparkerhenderson.com>
2020-05-18 16:23:46 -0700

    Add static files

--- src/main.rs
+++ src/main.rs
@@ -2,11 +2,33 @@
 
 #[macro_use] extern crate rocket;
 
+// Rocket makes it easy to serve static files. Use the StaticFiles custom 
+// handler from rocket_contrib, which makes it as simple as one line.
+//
+// Edit Cargo.toml and add:
+//
+//     rocket_contrib = "*"
+//
+// Edit main.rs and use the class:
+//
+//     use rocket_contrib::serve::StaticFiles;
+//
+// Edit the main fucntion and mount the route:
+//
+//     rocket.mount("/public", StaticFiles::from("/static"))
+//
+// See https://api.rocket.rs/v0.4/rocket_contrib/serve/struct.StaticFiles.html
+//
+use rocket_contrib::serve::StaticFiles;
+
 #[get("/")]
 fn index() -> &'static str {
     "Hello, world!"
 }
 
 fn main() {
-    rocket::ignite().mount("/", routes![index]).launch();
+    rocket::ignite()
+    .mount("/", routes![index])
+    .mount("/public", StaticFiles::from("/static"))
+    .launch();
 }
