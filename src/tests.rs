6189c632e5d2e9d5cf15d1babb14413edb8cb66d
Joel Parker Henderson <joel@joelparkerhenderson.com>
2020-05-21 15:59:56 -0700

    Add test for struct with invalid data

--- src/tests.rs
+++ src/tests.rs
@@ -54,13 +54,37 @@ fn create_item1_with_lenient_form() {
 }
 
 #[test]
-fn create_item2_with_form() {
+fn create_item_with_form_with_validation_with_valid_data() {
+    const VALID_DATA: i8 = 1;
     let client = Client::new(rocket()).expect("rocket");
     let mut response = client.post("/create-item2-with-form")
     .header(ContentType::Form)
-    .body(format!("name={}&star_count={}", "alice", 1))
+    .body(format!("name={}&star_count={}", "alice", VALID_DATA))
     .dispatch();
     assert_eq!(response.status(), Status::Ok);
     assert_eq!(response.content_type(), Some(ContentType::Plain));
     assert_eq!(response.body_string(), Some("Create item2 with form... name:alice star_count:1".into()));
 }
+
+#[test]
+fn create_item_with_form_with_validation_with_invalid_data() {
+    const INVALID_DATA: i8 = 0;
+    let client = Client::new(rocket()).expect("rocket");
+    let response = client.post("/create-item2-with-form")
+    .header(ContentType::Form)
+    .body(format!("name={}&star_count={}", "alice", INVALID_DATA))
+    .dispatch();
+    assert_eq!(response.status(), Status::UnprocessableEntity);
+}
+
+// #[test]
+// fn create_item3_with_json() {
+//     let client = Client::new(rocket()).expect("valid rocket instance");
+//     let mut response = client.post("/create-item3-with-lenient-form")
+//     .body("{name:\"alice\",complete:\"true\"}")
+//     .dispatch();
+//     assert_eq!(response.status(), Status::Ok);
+//     assert_eq!(response.content_type(), Some(ContentType::Plain));
+//     assert_eq!(response.body_string(), Some("Create item1 with form... name:alice complete:false".into()));
+// }
+
