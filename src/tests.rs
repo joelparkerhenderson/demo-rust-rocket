75e9b41264d0a6d58fd09e8c72e38175371139c4
Joel Parker Henderson <joel@joelparkerhenderson.com>
2020-05-21 11:25:47 -0700

    Add test for lenient form

--- src/tests.rs
+++ src/tests.rs
@@ -40,3 +40,15 @@ fn create_item1_with_form() {
     assert_eq!(response.content_type(), Some(ContentType::Plain));
     assert_eq!(response.body_string(), Some("Create item1 with form... name:alice complete:true".into()));
 }
+
+#[test]
+fn create_item1_with_lenient_form() {
+    let client = Client::new(rocket()).expect("rocket");
+    let mut response = client.post("/create-item1-with-lenient-form")
+    .header(ContentType::Form)
+    .body(format!("name={}", "alice"))
+    .dispatch();
+    assert_eq!(response.status(), Status::Ok);
+    assert_eq!(response.content_type(), Some(ContentType::Plain));
+    assert_eq!(response.body_string(), Some("Create item1 with lenient form... name:alice complete:false".into()));
+}
