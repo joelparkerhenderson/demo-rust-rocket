de71e23a7847d6ed807db39f729db445293d812a
Joel Parker Henderson <joel@joelparkerhenderson.com>
2020-05-21 20:00:25 -0700

    Reword

--- src/tests.rs
+++ src/tests.rs
@@ -4,15 +4,8 @@ use rocket::http::{ContentType, Status};
 
 #[test]
 fn hello() {
-    // Create a client for our Rocket instance. It's fine to use methods
-    // like expect and unwrap during testing: we want our tests to panic
-    // when something goes wrong.
     let client = Client::new(rocket()).expect("rocket");
-    // Create a new GET request to the "/" path, and dispatch it.
-    // We get back the application's response.
     let mut response = client.get("/").dispatch();
-
-    // Asserts
     assert_eq!(response.status(), Status::Ok);
     assert_eq!(response.content_type(), Some(ContentType::Plain));
     assert_eq!(response.body_string(), Some("hello world".into()));
