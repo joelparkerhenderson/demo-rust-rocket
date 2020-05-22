3cff0e5d7be2a6e50d1b0f89edec2cc3b189e72c
Joel Parker Henderson <joel@joelparkerhenderson.com>
2020-05-21 22:40:42 -0700

    Add Tera templates

--- src/main.rs
+++ src/main.rs
@@ -100,6 +100,58 @@ fn pages(path: PathBuf) -> Option<NamedFile> {
 
 use rocket_contrib::serve::StaticFiles;
 
+// Templates
+//
+// Rocket includes built-in templating support that works largely through a
+// Template responder in rocket_contrib. To render a template named "index", for
+// instance, you might return a value of type Template as follows:
+//
+//     use rocket_contrib::templates::Template;
+//
+//     #[get("/foo")]
+//     fn foo() -> Template {
+//         let context = HashMap::new(); /* object-like value */;
+//         Template::render("foo", &context)
+//     }
+//
+// Templates are rendered with the render method. The method takes in the name
+// of a template and a context to render the template with. The context can be
+// any type that implements Serialize and serializes into an Object value, such
+// as structs, HashMaps, and others.
+//
+// For a template to be renderable, it must first be registered. The Template
+// fairing automatically registers all discoverable templates when attached. The
+// Fairings sections of the guide provides more information on fairings. To
+// attach the template fairing, simply call .attach(Template::fairing()) on an
+// instance of Rocket as follows:
+//
+//     fn main() {
+//         rocket::ignite()
+//             .mount("/", routes![/* .. */])
+//             .attach(Template::fairing());
+//     }
+//
+// Rocket discovers templates in the configurable template_dir directory.
+// Templating support in Rocket is engine agnostic. The engine used to render a
+// template depends on the template file's extension. For example, if a file
+// ends with .hbs, Handlebars is used, while if a file ends with .tera, Tera is
+// used.
+//
+// Note: The name of the template does not include its extension.
+//
+// For a template file named index.html.tera, call render("index") and use the
+// name "index" in templates, i.e, {% extends "index" %} or {% extends "base" %}
+// for base.html.tera.
+
+use rocket_contrib::templates::Template;
+use std::collections::HashMap;
+
+#[get("/hello-with-template")]
+fn hello_with_template() -> Template {
+    let context: HashMap<String, String> = HashMap::new(); /* object-like value */;
+    Template::render("hello", &context)
+}
+
 // Cookies
 //
 // Cookies ia built-in request guard: it allows you to get, set, and 
@@ -306,10 +358,12 @@ fn upload(data: Data) -> Result<String, std::io::Error> {
 
 fn rocket() -> rocket::Rocket {
     rocket::ignite()
+    .attach(Template::fairing())
     .mount("/", routes![
         hello,
         echo,
-        pages, 
+        pages,
+        hello_with_template,
         get_cookie, 
         set_cookie, 
         create_item1_with_form, 
--- src/tests.rs
+++ src/tests.rs
@@ -39,6 +39,15 @@ fn files() {
 }
 
 #[test]
+fn hello_with_template() {
+    let client = Client::new(rocket()).expect("rocket");
+    let mut response = client.get("/hello-with-template").dispatch();
+    assert_eq!(response.status(), Status::Ok);
+    assert_eq!(response.content_type(), Some(ContentType::HTML));
+    assert_eq!(response.body_string(), Some("<!DOCTYPE html>\n<html>\n  <head>\n    <meta charset=\"utf-8\" />\n    <title>Demo Tera</title>\n  </head>\n  <body>\n    \n    hello world\n\n  </body>\n</html>\n".into()));
+}
+
+#[test]
 fn get_cookie() {
     let client = Client::new(rocket()).expect("rocket");
     let _ = client.get("/set-cookie").dispatch();
