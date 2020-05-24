d11d6dd3a0fc3da3cb4bca66b6f41b5c663b6376
Joel Parker Henderson <joel@joelparkerhenderson.com>
2020-05-23 18:19:11 -0700

    Update schema

--- src/schema.rs
+++ src/schema.rs
@@ -1,10 +1,9 @@
 table! {
     items (id) {
-        id -> Uuid,
+        id -> Int4,
         demo_boolean -> Nullable<Bool>,
-        demo_integer -> Nullable<Int4>,
+        demo_int -> Nullable<Int4>,
         demo_json -> Nullable<Json>,
-        demo_numeric -> Nullable<Numeric>,
         demo_text -> Nullable<Text>,
         demo_timestamp -> Nullable<Timestamp>,
     }
